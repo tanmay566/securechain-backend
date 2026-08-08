24ae7148037dbb2650000585145022d2d886156e
atman-onit <atman.bharadwaj06@gmail.com>
2026-08-08 20:24:53 +0530

    core chain

--- blockchain.py
+++ blockchain.py
@@ -1,22 +1,40 @@
 import hashlib, json, time
-class Asset():
-    def __init__(self, name, asset_id, asset_type, expiry, 
-                 temp_constraint, status, created_at,
-                 manufacturer, start_loc, end_loc):
-        self.name = name
-        self.asset_if = asset_id
-        self.asset_type = asset_type
-        self.expiry = expiry
-        self.temp_constraint = temp_constraint
-        self.status = status
-        self.created_at = created_at
-        self.manufacturer = manufacturer
-        self.start_loc = start_loc
-        self.end_loc = end_loc
-    
-class block():
-    pass
 
+    
+class Block:
+    def __init__(self, index: int, data: dict, prev_hash: str, timestamp: float = None):
+        self.index = index
+        self.timestamp = timestamp or time.time()
+        self.data = data #passed in dict/json format
+        self.prev_hash = prev_hash
+        self.hash = self.compute_hash()
+
+    def compute_hash(self) -> str:
+        block_string = json.dumps({
+            "index": self.index,
+            "timestamp": self.timestamp,
+            "data": self.data,
+            "prev_hash": self.prev_hash
+        }, sort_keys=True)
+        return hashlib.sha256(block_string.encode()).hexdigest() #hash
+
+    def to_dict(self) -> dict:
+        return {
+            "index": self.index,
+            "timestamp": self.timestamp,
+            "data": self.data,
+            "prev_hash": self.prev_hash,
+            "hash": self.hash
+        }
+
+    @staticmethod
+    def from_dict(d: dict) -> "Block":
+        block = Block(d["index"], d["data"], d["prev_hash"], d["timestamp"])
+        block.hash = d["hash"]  # trust stored hash when loading; is_valid() will catch tampering
+        return block
+  
 class blockchain():
     pass
+
+
 
--- schemas.py
+++ schemas.py
@@ -7,9 +7,14 @@ class AssetCreate(BaseModel):
     asset_id:str
     asset_type :str
     registration_datetime : str 
-    temp_constraint: Optional[str] = None
+    temp_constraint: Optional[Temp_constraint] 
     manufacturer : Optional[str] = None
     manufacturing_date: Optional[date] = None
-    expiry_date: Optional[date] = None
+    expiry: Optional[date] = None
     origin: Optional[str] = None
+
+class Temp_constraint(BaseModel):
+    min_temp = float
+    max_temp = float
+    
     
