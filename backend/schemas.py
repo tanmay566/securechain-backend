dcbb3a1e7604bcc80f0782d086ce6b9e97a64973
atman-onit <atman.bharadwaj06@gmail.com>
2026-08-09 00:04:24 +0530

    mino tweaks

--- backend/schemas.py
+++ backend/schemas.py
@@ -12,7 +12,7 @@ class AssetCreate(BaseModel):
     asset_id:str
     asset_type :str
     registration_datetime : str 
-    temp_constraint: Optional[temp_constraint] 
+    temp_constraint: Optional[Temp_constraint] 
     manufacturer : Optional[str] = None
     manufacturing_date: Optional[date] = None
     expiry: Optional[date] = None
